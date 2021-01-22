f0128d6a783f54d335babfefbeaeed75979b5a0a
Artsiom Trapezon <atrapezon@gmail.com>
2021-01-22 12:04:50 +0300

    Fix threads optional task

--- src/main/java/threads/optionaltask/PlaneThread.java
+++ src/main/java/threads/optionaltask/PlaneThread.java
@@ -1,37 +1,29 @@
 package threads.optionaltask;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class PlaneThread extends Thread{
 
-    private Runway[] runways;
+    private ArrayBlockingQueue<Runway> runways;
 
-    public void setRunways(Runway[] runways) {
+    public void setRunways(ArrayBlockingQueue<Runway> runways) {
         this.runways = runways;
     }
 
     @Override
     public void run() {
-        boolean flagOfEnd;
-        flagOfEnd = false;
-        while (!flagOfEnd) {
-            for (Runway runway : runways) {
-                if (runway.lock.tryLock()) {
-                    runway.startInfo();
-                    System.out.println(getName() + " начал выход на полосу");
-                    try {
-                        TimeUnit.SECONDS.sleep(3);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(getName() + " взлетел");
-                    runway.endInfo();
-                    runway.condition.signalAll();
-                    runway.lock.unlock();
-                    flagOfEnd = true;
-                    break;
-                }
-            }
+        Runway runwayTemp;
+        try {
+            runwayTemp = runways.take();
+            runwayTemp.startInfo();
+            System.out.println(getName() + " вышел на полосу");
+            TimeUnit.SECONDS.sleep(3);
+            System.out.println(getName() + " взлетел");
+            runwayTemp.endInfo();
+            runways.put(runwayTemp);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/threads/optionaltask/Runner.java
+++ src/main/java/threads/optionaltask/Runner.java
@@ -1,16 +1,19 @@
 package threads.optionaltask;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Runner {
     public static void main(String[] args) {
-        Runway[] runways = new Runway[5];
-        for (int i = 0; i < runways.length; i++) {
-           runways[i] = new Runway( "Полоса №" + i);
+        ArrayBlockingQueue<Runway> runways = new ArrayBlockingQueue<>(5);
+        int runwaysCapacity = runways.remainingCapacity();
+        for (int i = 0; i < runwaysCapacity; i++) {
+           runways.add(new Runway("Полоса " + (i+1)));
         }
 
         for (int i = 0; i < 10; i++) {
             PlaneThread plane = new PlaneThread();
             plane.setRunways(runways);
-            plane.setName("plane " + i);
+            plane.setName("Самолет " + (i+1));
             plane.start();
         }
     }
